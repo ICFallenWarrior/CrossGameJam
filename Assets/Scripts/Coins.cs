0c5d5e19dd248865f189e87bd7500acb544eff01
ICFallenWarrior <pedromourinha.playz@gmail.com>
2022-02-10 13:14:41 +0000

    Coins
    Not working properly yet(will be soon)

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class Coins : MonoBehaviour
+{
+    public float coins;
+    public Text coinDisplayer;
+
+    void Awake()
+    {
+        coinDisplayer.text = "Coins: " + coins.ToString();
+    }
+
+    private void Update()
+    {
+        CoinsCheat();
+        UpdateCoins();
+    }
+
+    public void CoinsCheat()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            getMoney(100);
+            coinDisplayer.text = "Coins: " + coins;
+        }
+    }
+
+    public void UpdateCoins()
+    {
+
+            coinDisplayer.text = "Coins: " + coins;
+
+    }
+
+    public void spendMoney(float cost){
+        coins -= cost;
+    } 
+
+    public void getMoney(float stonks){
+        coins += stonks;
+    }
+}
+
